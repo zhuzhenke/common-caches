6bf88af55c65e69c760ad5b35b08e73c0018cfd9
zhuzhenke <zhenke.zhu@lazada.com>
2019-02-19 11:54:04 +0800

    use concurrent

--- google-guava/src/test/java/com/google/guava/cache/GuavaCacheRefreshTest.java
+++ google-guava/src/test/java/com/google/guava/cache/GuavaCacheRefreshTest.java
@@ -5,6 +5,7 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import lombok.Data;
 import org.junit.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -17,7 +18,15 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * @date 2019/2/17
  */
 public class GuavaCacheRefreshTest {
-    Integer i = 0;
+
+    @Data
+    public class SkuCache {
+        private String skuId;
+        private String skuCode;
+        private Long realQuantity;
+    }
+
+    AtomicInteger loadTimes = new AtomicInteger(0);
     AtomicInteger count = new AtomicInteger(0);
 
     @Test
@@ -25,12 +34,12 @@ public class GuavaCacheRefreshTest {
         LoadingCache<String, SkuCache> loadingCache = CacheBuilder.newBuilder()
                 .refreshAfterWrite(1000, TimeUnit.MILLISECONDS)
                 //Prevent data reloading from failing, but the value of memory remains the same
-                .expireAfterWrite(2500, TimeUnit.MILLISECONDS)
+                .expireAfterWrite(1500, TimeUnit.MILLISECONDS)
                 .build(new CacheLoader<String, SkuCache>() {
                     @Override
                     public SkuCache load(String key) {
                         SkuCache skuCache = new SkuCache();
-                        skuCache.setSkuCode(key + "---" + (++i));
+                        skuCache.setSkuCode(key + "---" + (loadTimes.incrementAndGet()));
                         skuCache.setSkuId(key);
                         skuCache.setRealQuantity(100L);
                         System.out.println("load..." + key);
@@ -41,23 +50,20 @@ public class GuavaCacheRefreshTest {
                     public ListenableFuture<SkuCache> reload(String key, SkuCache oldValue) throws Exception {
                         checkNotNull(key);
                         checkNotNull(oldValue);
-                        System.out.println("reload..." + (++i));
+                        System.out.println("reload...");
                         //Simulate time consuming operation
-                        Thread.sleep(1000);
+//                        Thread.sleep(1000);
                         return Futures.immediateFuture(load(key));
                     }
                 });
 
 
         for (int i = 0; i < 1000; i++) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        getValue(loadingCache);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+            new Thread(() -> {
+                try {
+                    getValue(loadingCache);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }).start();
         }
@@ -68,7 +74,7 @@ public class GuavaCacheRefreshTest {
 
 
     private void getValue(LoadingCache<String, SkuCache> loadingCache) throws Exception {
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < 10; i++) {
             Thread.sleep(300l);
             System.out.println(loadingCache.get("sku").toString() + " - " + count.incrementAndGet());
         }
